09f5664853318be358ec02ad3a41928b0e838e54
mattdesl <dave.des@gmail.com>
2015-02-20 10:41:17 -0500

    getting a test into place, updating bin field

--- test.js
+++ test.js
@@ -7,11 +7,11 @@ var ndarray = require('ndarray')
 
 test("generates a signed distance field from RGBA pixels", function(t) {
     document.body.style.background = '#1d1d1d'
-    var img = load('lato.png', function(err, img) {
+    var img = load('img.png', function(err, img) {
         var px = getPixels(img)
         var array = ndarray(px, [img.width, img.height, 4])
-
-        var result = imageSdf(array, { spread: 10, downscale: 2 })
+        
+        var result = imageSdf(array, { spread: 10, downscale: 1 })
 
         var cnv = document.createElement('canvas')
         var ctx = cnv.getContext('2d')
